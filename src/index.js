df009efedb75e0be4f7ba3baea2368882575e1e5
Teodor Samoniuc <samoniucteodor@yahoo.com>
2021-07-16 18:45:51 +0300

    adding files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Songs from "./Songs";
 import Artists from "./Artists";
+import Albums from "./Albums";
+import Genres from "./Genres";
 
 class App extends Component {
     render() {
@@ -13,6 +15,8 @@ class App extends Component {
                         <Route exact path="/" component={Home} />
                         <Route path="/songs" component={Songs} />
                         <Route path="/artists" component={Artists} />
+                        <Route path="/albums" component={Albums} />
+                        <Route path="/genres" component={Genres} />
                     </Switch>
                 </Router>
         );
